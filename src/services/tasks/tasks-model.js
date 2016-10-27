b65a60498138082dfa9195f20a18136f16efad9f
Aengus <aengus.hearne@gmail.com>
2016-10-27 18:03:23 -0400

    Change allowed title lengths

--- src/services/tasks/tasks-model.js
+++ src/services/tasks/tasks-model.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const tasksSchema = new Schema({
-  title: { type: String, required: true, maxlength: 14 },
+  title: { type: String, required: true, maxlength: 24 },
   completed: { type: Boolean, default: false },
   totalTime: { type: Number , 'default': 0 },
   startTime: { type: Date },
